b421d5a053da8acde5f1f8b86c08e225e124cc7d
JoshuamURD <103409475+JoshuamURD@users.noreply.github.com>
2023-02-02 07:58:07 +0000

    Started the excel functionality. Will eventually merge with main program.

---
+++ Excel functionality.py
@@ -0,0 +1,23 @@
+import xlsxwriter
+
+class Workbook():
+    def __init__(self):
+        self.workbook = xlsxwriter.Workbook('Board directors.xlsx')
+
+    def createWorksheet(self):
+        self.worksheet = self.workbook.add_worksheet()
+
+    def create_worksheet_headings(self):
+        col = 0
+        headings = ["Company Name", "ASX Code", "Board director Name", "Board Director Title", "pay", "exercised", 
+        "year born", "gender", "Sector", "Industry", "Company Description", "Adresss", "Postcode"]
+        for heading in headings:
+            self.worksheet.write(0, col, heading)
+            col+=1
+
+    def close_spreadsheet(self):
+        self.workbook.close()
+i = Workbook()
+i.createWorksheet()
+i.create_worksheet_headings()
+i.close_spreadsheet()
